f29ee3da696af2b0a94c854e4d5f313553a3d937
Yll Cervadiku <yll.cervadiku@gmail.com>
2025-04-08 13:39:55 +0200

    Added encrypt method to GronsfeldCipher class

--- src/GronsfeldCipher.java
+++ src/GronsfeldCipher.java
@@ -1,2 +1,22 @@
 public class GronsfeldCipher {
+    private static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    public String encrypt(String message, String key) {
+        message = message.toUpperCase();
+        StringBuilder result = new StringBuilder();
+        int keyIndex = 0;
+
+        for (char c : message.toCharArray()) {
+            if (Character.isLetter(c)) {
+                int shift = Character.getNumericValue(key.charAt(keyIndex % key.length()));
+                int charIndex = ALPHABET.indexOf(c);
+                int newIndex = (charIndex + shift) % 26;
+                result.append(ALPHABET.charAt(newIndex));
+                keyIndex++;
+            } else {
+                result.append(c);
+            }
+        }
+        return result.toString();
+    }
 }
